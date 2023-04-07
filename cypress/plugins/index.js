f2bc2a2b39590580394d74280267ada33ddebf23
Bernardo Oliveira <beernardoliveira@gmail.com>
2023-04-07 12:39:06 -0300

    ajuste index.js

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -16,6 +16,4 @@
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
-module.exports = (on, config) => {
-  require('cypress-grep/src/plugin')(config)
-}
+module.exports = (on, config) => {}
